e63a74f48b605e7cb62f70121ae94af5af06402e
Michael H. Arieli <michaelharieli@gmail.com>
2021-04-23 14:26:38 -0700

    Updated modified-values to support checkboxes and bump version 2.0.181

--- src/js/lib/utils/modified_values.js
+++ src/js/lib/utils/modified_values.js
@@ -48,7 +48,11 @@ export function modifiedValues($drawer, field, value, $container = $('body')) {
   if ($form.length) {
     const $field = $form.find(`[id$="_${field}"]`);
     if ($field.length && $field.closest('.dropdown').length === 0) {
-      $field.val(value);
+      if ($field.is(':checkbox')) {
+        $field.prop('checked', value);
+      } else {
+        $field.val(value);
+      }
     }
   }
 
